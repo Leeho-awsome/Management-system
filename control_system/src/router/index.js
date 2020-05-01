c78c9b9408b01f905f959e71dbe9aaa4a51d9108
Leeho-awsome <1394415223@qq.com>
2020-05-01 11:48:27 +0800

    分支登录部分

--- control_system/src/router/index.js
+++ control_system/src/router/index.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+//@ webpack 会自动 找到src
+import login from "@/components/login/login.vue"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-     
+      name:"login",
+      path: '/login',
+      component:login
+      
     }
   ]
 })
